299c8a09ce92b73843eb3072118721d56ced36fd
sohelazure <sohel.shaik@sailssoftware.com>
2025-11-04 14:33:23 +0530

    Trigger feature branch build

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("I,m Sohel");
+        System.out.println("I,m Sohel. speed 360kmph. memory 2KB");
         
 
     }
